905abfac30450687ece29690c875a7eab58fae96
ttltna <ttltnaaa@gmail.com>
2022-02-09 23:53:58 +0900

    Fix: 에러사항 수정중

--- src/js/Components/app.js
+++ src/js/Components/app.js
@@ -1,8 +1,7 @@
 import '../new/guest/find_user'
-<<<<<<< HEAD
+
 import '../new/guest/join'
-=======
+
 import '../new/util/main'
+
 import '../new/guest/login'
->>>>>>> 3916d02f2478082e415c1ca8b1832dda86620bf8
-// console.log('app component')
