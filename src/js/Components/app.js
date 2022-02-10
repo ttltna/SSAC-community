155d5668440dab0b550bf521d0434720dfe8e5d0
D.seok <82019373+Dseok12@users.noreply.github.com>
2022-02-10 22:36:44 +0900

    Chore: app.js 루트 경로 지정 (#52)
    * Chore: app.js 루트 경로 지정

--- src/js/Components/app.js
+++ src/js/Components/app.js
@@ -1,7 +1,13 @@
 import '../new/guest/find_user'
-
 import '../new/guest/join'
+import '../new/guest/login'
+
+import '../new/user/my_page'
 
 import '../new/util/main'
+import '../new/util/succeed'
 
-import '../new/guest/login'
+import '../new/board/evaluation'
+import '../new/board/general_forum'
+import '../new/board/recruit_project'
+
